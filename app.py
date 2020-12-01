7f9989e50c54e72a2b1ef155b3eccd5ecb7a31ec
entirelymagic <entirelymagic@gmail.com>
2020-12-01 18:32:08 +0200

    commented

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ from data.data_types import Users, Heroes, Items
 Database.initialise(database=database_name, user=user_name, password=user_password, host=host_name, port=5432)
 
 
-first_char = Heroes('Godlike', 'elvis.munteanu@gmail.com')
-wood_helmet = Items('Wood helmet', first_char.hero_name, 'helmet', 0)
-wood_helmet.create_new_item()
-first_char.load_from_db_by_char_name(first_char.hero_name)
-print(first_char)
-items = Items.load_item_from_db_by_hero_name(first_char.hero_name)
-print(items)
+# first_char = Heroes('Godlike', 'elvis.munteanu@gmail.com')
+# wood_helmet = Items('Wood helmet', first_char.hero_name, 'helmet', 0)
+# wood_helmet.create_new_item()
+# first_char.load_from_db_by_char_name(first_char.hero_name)
+# print(first_char)
+# items = Items.load_item_from_db_by_hero_name(first_char.hero_name)
+# print(items)
 
 
 Database.close_all_connections()
-#
