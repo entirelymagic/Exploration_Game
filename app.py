ee3f4ec62eaf1e4e7e1b356c63c3b0947329310a
entirelymagic <entirelymagic@gmail.com>
2020-12-03 12:46:01 +0200

    Updated testing.

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 from database.postgreSQL_connection import Database
 from database.db_login_info import database_name, user_name, user_password, host_name
 from data.data_types import Users, Heroes, Items, HeroSlots
-from data.manipulate_items import CreateItems, HeroStats
+from data.manipulate_items import CreateItems, HeroStats, CreateMonster
 from random import randint
 
 # Initialize database connection
-Database.initialise(database=database_name, user=user_name, password=user_password, host=host_name, port=5432)
+Database.initialise(database=database_name,
+                    user=user_name,
+                    password=user_password,
+                    host=host_name,
+                    port=5432)
 
 USER_CHOICE = """
 1. Start Adventure
 2. View Inventory
 3. View HeroStats
 """
+my_hero = HeroStats('Godlike', 'elvis.munteanu@gmail.com')
 
-slots_for_my_hero = HeroSlots('Godlike')
-print(slots_for_my_hero)
-
-slots_after_db = slots_for_my_hero._get_hero_slots_from_db()
-print(slots_for_my_hero)
-
-slots_for_my_hero._update_slots_db()
-
-print(slots_for_my_hero)
+print(my_hero.hero_slots)
+print(my_hero)
 
 Database.close_all_connections()
 
--- testing/test.py
+++ testing/test.py
@@ -2,9 +2,44 @@ from unittest import TestCase
 from app import *
 
 
+def startConn(test):
+    def wrapper(*args, **kwargs):
+        Database.initialise(database=database_name,
+                                    user=user_name,
+                                    password=user_password,
+                                    host=host_name,
+                                    port=5432)
+        test(*args, **kwargs)
+        Database.close_all_connections()
+
+    return wrapper
+
+
 class Testing(TestCase):
+    @startConn
     def test_HeroSlots(self):
         slots_for_my_hero = HeroSlots('Godlike')
-        printer = print(slots_for_my_hero)
-        self.assertEquals(print(slots_for_my_hero), printer)
+        # print(slots_for_my_hero)
+        slots_after_db = slots_for_my_hero.get_hero_slots_from_db()
+        slots_for_my_hero.update_slots_db()
+        print(slots_for_my_hero)
+        self.assertNotEqual(slots_for_my_hero, slots_after_db)
 
+    @startConn
+    def test_HeroStats(self):
+        my_hero = HeroStats('Godlike', 'elvis.munteanu@gmail.com')
+        print(my_hero.items)
+        print(my_hero.hero_slots)
+        print(my_hero)
+
+    def test_CreateItems(self):
+        for i in (10, 30, 100):
+            new_item = CreateItems(i, 0)
+            new_item3 = CreateItems(i, 1)
+            # print(new_item)
+            # print(new_item3)
+
+    def test_CreateMonsters(self):
+        for i in (10, 100):
+            new_item = CreateMonster(i)
+            # print(new_item)
