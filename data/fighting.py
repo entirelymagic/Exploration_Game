7fbc204cc419a2f86419f4273201622e27a5d9d4
entirelymagic <entirelymagic@gmail.com>
2020-12-03 12:44:40 +0200

    Started Fighting file with Fighting class

--- data/fighting.py
+++ data/fighting.py
@@ -1,6 +1,6 @@
-class Fignting:
+class Fighting:
     def __init__(self, hero, monster):
-        self.winner = self.__winner()
+
         self.hero = hero
         self.monster = monster
         # TODO finish class
